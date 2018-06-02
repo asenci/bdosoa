b7102140b3279a98b24ff8abd689e06f63b54520
Andre Sencioles <asenci@gmail.com>
2018-06-02 13:56:41 +1200

    Migrate to GitHub

--- bdosoa/__init__.py
+++ bdosoa/__init__.py
@@ -5,9 +5,9 @@ bdosoa - Brazil LNP (NPAC) BDO/SOA messages processing
 
 __title__ = 'bdosoa'
 __summary__ = 'Brazil LNP (NPAC) BDO/SOA messages processing'
-__url__ = 'http://bitbucket.org/asenci/bdosoa'
+__url__ = 'https://github.com/asenci/bdosoa'
 
 __version__ = '0.2'
 
-__author__ = 'Andre Sencioles Vitorio Oliveira'
-__email__ = 'andre@bcp.net.br'
+__author__ = 'Andre Sencioles'
+__email__ = 'asenci@gmail.com'
